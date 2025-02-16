5d0a14688eadd5b0351ee93fc2b8f64466db7e68
Nitin Sharma <nitinsharma9829547681@gmail.com>
2025-02-16 19:35:44 +0530

    echo

--- Scripts/02-echo.sh
+++ Scripts/02-echo.sh
@@ -1,25 +1,26 @@
 #!/bin/bash
 
-# Trying out the echo command in different scenerios
-# echo commands prints on the terminal screen
+# Trying out the echo command in different scenarios
+# echo commands prints its arguments on the terminal screen
+# It separated all it arguments by a white-space
 
-echo Hello World
 echo "Hello World"
-echo "Hello  World"
-echo Hello  World
+echo Hello World
+echo "Hello   World"
+echo Hello   World
 echo "Hello \n World"
 echo -e "Hello \n world" # Use -e flag to render escape sequence characters
-echo Hello * World
+echo Hello * World  # '*' is a wildcard, it prints all the files in the current directory
 echo "Hello * World"
 echo "Hello" "  " "World"
 echo Hello "  " World
 echo Hello "World"
 echo "Hello" World
 echo "Hello "*" World" # Interesting, Why??
-echo `hello world` # Backtick is used for commands, I guess
-echo `hello` world
 echo 'hello world' # Single quotes same as double quotes, Any differences?
 echo 'hello' world
+echo `hello world` # Backtick is used for commands, I guess
+echo `hello` world
 echo echo
 
 # Also even when there is an error when I used backtick and command is not found by the bash, the script execution still continues, Good to know!
