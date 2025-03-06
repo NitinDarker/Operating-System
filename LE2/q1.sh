addb1b4ddb44b6b66202213bdab37ab935348d5a
NitinDarker <nitinsharma9829547681@gmail.com>
2025-03-06 10:43:49 +0530

    q1 done

--- LE2/q1.sh
+++ LE2/q1.sh
@@ -2,7 +2,6 @@
 
 # Que : 1. Write down a shell script which lists top 10 processes using highest amount of main memory.
 
-echo "Top 10 process with high memory usages"
-printf "%-10s %-10s %-15s %-s\n" "PID" "USER" "MEM" "COMMAND"
+echo "Top 10 process with high memory usages:"
 echo "-------------------------------------------"
 ps -eo pid,user,%mem,cmd --sort=-%mem | head -11
