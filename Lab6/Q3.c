5cb248bfc249d8fbdb6ef81e66eb4d87eb56a843
Nitin Sharma <nitinsharma9829547681@gmail.com>
2025-04-23 17:01:47 +0530

    LE8 Q3

--- Lab6/Q3.c
+++ Lab6/Q3.c
@@ -1,4 +1,66 @@
+#include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <pthread.h>
+#include <unistd.h>
 
+#define NUM_EARLY 3
+#define NUM_LATE 2
+
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+pthread_cond_t cond = PTHREAD_COND_INITIALIZER;
+int early_completed = 0;
+
+void *thread_func(void *arg) {
+    int type = *(int *)arg;
+    free(arg);
+
+    if (type == 0) { // Early thread
+        printf("Early thread %lu starting\n", (unsigned long)pthread_self());
+
+        // Simulate work
+        sleep(1);
+
+        pthread_mutex_lock(&mutex);
+        early_completed++;
+        if (early_completed == NUM_EARLY) {
+            pthread_cond_broadcast(&cond);
+        }
+        pthread_mutex_unlock(&mutex);
+
+        printf("Early thread %lu completed\n", (unsigned long)pthread_self());
+    } else { // Late thread
+        pthread_mutex_lock(&mutex);
+        while (early_completed < NUM_EARLY) {
+            pthread_cond_wait(&cond, &mutex);
+        }
+        pthread_mutex_unlock(&mutex);
+
+        printf("Late thread %lu starting\n", (unsigned long)pthread_self());
+
+        // Simulate work
+        sleep(1);
+
+        printf("Late thread %lu completed\n", (unsigned long)pthread_self());
+    }
+
+    return NULL;
+}
+
+int main() {
+    pthread_t threads[NUM_EARLY + NUM_LATE];
+
+    // Create all threads simultaneously
+    for (int i = 0; i < NUM_EARLY + NUM_LATE; i++) {
+        int *arg = malloc(sizeof(int));
+        *arg = (i < NUM_EARLY) ? 0 : 1;
+        pthread_create(&threads[i], NULL, thread_func, arg);
+    }
+
+    // Wait for all threads to complete
+    for (int i = 0; i < NUM_EARLY + NUM_LATE; i++) {
+        pthread_join(threads[i], NULL);
+    }
+
+    printf("All threads completed\n");
+    return 0;
+}
